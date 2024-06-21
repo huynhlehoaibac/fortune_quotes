1f658b23c13b2a832449124322a32985bfdd991d
huynhlehoaibac <huynhlehoaibac@gmail.com>
2024-06-21 05:25:22 -0400

    Make it available to zsh

---
+++ fortune.plugin.zsh
@@ -0,0 +1,27 @@
+function fortune {
+    # We convert the .json file into an array.
+    declare -A quotearray
+    while IFS="=" read -r key value 
+    do
+        quotearray[$key]="$value"
+    done < <(jq -r 'to_entries|map("\(.key)=\(.value)")|.[]' $ZSH/custom/plugins/fortune/fortune/fortune.json)
+
+    # We get a random number between 0 and the length of the array and we get the quote from the array.
+    quotetoprint="${quotearray[$((RANDOM % ${#quotearray[@]}))]}"
+    # # We remove the { and } from the quote
+    quotetoprint="${quotetoprint//\"}"
+    # # We remove the "quoteText" and "quoteAuthor" from the quote
+    quotetoprint="${quotetoprint//quoteText:}"
+    quotetoprint="${quotetoprint//,quoteAuthor:/\n--}"
+    # # We remove the { and } from the quote
+    quotetoprint="${quotetoprint//\{}"
+    quotetoprint="${quotetoprint//\}}"
+    # We print the quote
+    echo "$quotetoprint"
+}
+
+# Uncomment the following line to get a quote when opening your Gitbash terminal
+# fortune
+
+# Uncomment the following line if you already have "cowsay" installed and want to combine it with the fortune quote
+fortune | cowsay 
